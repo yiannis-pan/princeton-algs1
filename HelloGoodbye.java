4aeed6e84bf783c2eb40486747b643cd6b28502c
Yiannis Panagidis <ypanagidis@gmail.com>
2023-04-07 20:55:09 +0300

    finished HelloGoodbye

--- HelloGoodbye.java
+++ HelloGoodbye.java
@@ -1,2 +1,11 @@
-package PACKAGE_NAME;public class HelloGoodbye {
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
+public class HelloGoodbye {
+    public static void main(String args[]) {
+        System.out.println("Hello" + " " + args[0] + " " + "and" + " " + args[1]);
+        System.out.println("Goodbye" + " " + args[1] + " " + "and" + " " + args[0]);
+    }
 }
+
+
