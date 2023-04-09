256c4e6194972c9c35e4b3ee2b11e6983e6c36b9
Yiannis Panagidis <ypanagidis@gmail.com>
2023-04-09 16:22:21 +0300

    finished

--- week1/quckFIndLecture/quickFindLecture/src/Main.java
+++ week1/quckFIndLecture/quickFindLecture/src/Main.java
@@ -1,7 +1,12 @@
 import edu.princeton.cs.algs4.StdIn.*;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        QuickFindUF UF = new QuickFindUF(9);
+        UF.union(3, 4);
+        UF.union(3, 8);
+        System.out.println(UF.isConnected(3,4));
+        System.out.println(UF.isConnected(2,1));
+        UF.printID();
     }
 }
 
--- week1/quckFIndLecture/quickFindLecture/src/QuickFindUF.java
+++ week1/quckFIndLecture/quickFindLecture/src/QuickFindUF.java
@@ -1,11 +1,29 @@
+
 public class QuickFindUF {
     private int[] id;
 
     public QuickFindUF(int N){
-    id = new int[N];
-    for (int i = 0; i< N; i++){
-        id[i] = i;
+        id = new int[N];
+        for (int i = 0; i< N; i++){
+            id[i] = i;
+        }
     }
 
+    public void union(int p, int q) {
+        int idAtP = this.id[p];
+        int idAtQ = this.id[q];
+        for (int i = 0; i < this.id.length; i++) {
+           if (this. id[i] == idAtP) id[i] = idAtQ;
+        }
+    }
+
+    public boolean isConnected(int p, int q) {
+        return this.id[p] == this.id[q];
+    }
+
+    public void printID() {
+      for (int i = 0; i< this.id.length; i++) {
+          System.out.println("Index: " + i + " " + "ID: " + this.id[i]);
+      }
     }
 }
